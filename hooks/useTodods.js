610dd4860f084dceb909ea62584a87bf4102ceed
Răzvan Sbîngu <razvan.sbingu@thinslices.com>
2021-11-02 11:22:44 +0200

    action names refactor

--- hooks/useTodods.js
+++ hooks/useTodods.js
@@ -1,30 +1,30 @@
 import * as React from "react";
 
 const actions = {
-  ADD_TODO: "ADD_TODO",
-  DELETE_TODO: "DELETE_TODO",
-  CHECK_TODO: "CHECK_TODO",
-  UNCHECK_TODO: "UNCHECK_TODO",
-  CLEAR_TODOS: "CLEAR_TODOS",
+  ADD: "ADD",
+  CHECK: "CHECK",
+  DELETE: "DELETE",
+  UNCHECK: "UNCHECK",
+  CLEAR_ALL: "CLEAR_ALL",
 };
 
 const initialState = [];
 
 function reducer(state, action) {
   switch (action.type) {
-    case actions.ADD_TODO:
+    case actions.ADD:
       return [...state, action.payload];
-    case actions.DELETE_TODO: {
+    case actions.DELETE: {
       const id = action.payload.id;
       const index = state.findIndex(todo => todo.id === id);
 
       return [...state.slice(0, index), ...state.slice(index + 1)];
     }
-    case actions.CHECK_TODO:
-    case actions.UNCHECK_TODO: {
+    case actions.CHECK:
+    case actions.UNCHECK: {
       const id = action.payload.id;
       const index = state.findIndex(todo => todo.id === id);
-      const isCheckAction = action.type === actions.CHECK_TODO;
+      const isCheckAction = action.type === actions.CHECK;
 
       return [
         ...state.slice(0, index),
@@ -32,7 +32,7 @@ function reducer(state, action) {
         ...state.slice(index + 1),
       ];
     }
-    case actions.CLEAR_TODOS:
+    case actions.CLEAR_ALL:
       return initialState;
     default:
       throw new Error(`🚨 Action type ${action} is not defined`);
@@ -44,11 +44,11 @@ export default function useTodos() {
 
   const handlers = React.useMemo(
     () => ({
-      addTodo: todo => dispatch({ type: actions.ADD_TODO, payload: todo }),
-      deleteTodo: id => dispatch({ type: actions.DELETE_TODO, payload: { id } }),
-      checkTodo: id => dispatch({ type: actions.CHECK_TODO, payload: { id } }),
-      uncheckTodo: id => dispatch({ type: actions.UNCHECK_TODO, payload: { id } }),
-      clearTodos: () => dispatch({ type: actions.CLEAR_TODOS }),
+      addTodo: todo => dispatch({ type: actions.ADD, payload: todo }),
+      deleteTodo: id => dispatch({ type: actions.DELETE, payload: { id } }),
+      checkTodo: id => dispatch({ type: actions.CHECK, payload: { id } }),
+      uncheckTodo: id => dispatch({ type: actions.UNCHECK, payload: { id } }),
+      clearTodos: () => dispatch({ type: actions.CLEAR_ALL }),
     }),
     []
   );
